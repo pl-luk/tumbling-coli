5d7c42d249b6fa1e3baf98293c70f567196a8a8f
pl-luk <lukas@smart-ies.de>
2024-01-15 12:54:46 +0100

    Fix typo

--- src/bacteria.py
+++ src/bacteria.py
@@ -4,7 +4,7 @@ class bacteria:
 
     def __init__(self):
 
-        self.looking_at = np.array([1., 0., 0.], dtype = np.float64)
+        self.looking_at = np.array([1., 0.], dtype = np.float64)
         self.pos = np.array([0., 0.], dtype = np.float64)
 
         self.mean_angle = 70 * np.pi / 180 # angle in rad
